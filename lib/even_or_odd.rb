6f5b832aa985643cb2624e2e6cb626f4cb2a7431
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-08 19:45:09 +0000

    hard coded 1

--- lib/even_or_odd.rb
+++ lib/even_or_odd.rb
@@ -1,5 +1,7 @@
 def even_or_odd(number)
   if number == 8 
     return 'Even'
+  elsif number == 1 
+    return 'Odd'
   end
 end
--- spec/even_or_odd_spec.rb
+++ spec/even_or_odd_spec.rb
@@ -4,7 +4,11 @@ require_relative '../lib/even_or_odd'
 # "Even" for even numbers or "Odd" for odd numbers.
 
 describe 'even or odd' do 
-  it 'should returns even when given the number 2' do
+  it 'should return even when given the number 2' do
     expect(even_or_odd(8)).to eq 'Even'
+  end
+
+  it 'should return odd when given the number 1' do
+    expect(even_or_odd(1)).to eq 'Odd'
   end
 end
